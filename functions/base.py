696bba99485252a62ee151918ee986a749259a8a
GRParasky <52867650+GRParasky@users.noreply.github.com>
2020-02-29 19:57:05 -0300

    class Base

---
+++ functions/base.py
@@ -0,0 +1,16 @@
+class Base:
+    def __init__(self):
+        self.x = 0
+        self.y = 1
+
+    def setX(self, x):
+        self.x = x
+
+    def getX(self):
+        return self.x
+
+    def setY(self, y):
+        self.y = y
+
+    def getY(self):
+        return self.y
